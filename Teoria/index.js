2282ba6ba3beca41526091e0c5c65c112d2108e6
alex2001bis <alex2001bis@gmail.com>
2022-10-17 14:19:42 +0200

    Fin teoria

--- Teoria/index.js
+++ Teoria/index.js
@@ -1,4 +1,4 @@
-console.log("Hola mundo soy un console log");
+/*console.log("Hola mundo soy un console log");
 let variable=true;
 console.log(variable);
 const constante=5;
@@ -51,3 +51,53 @@ let contador2=12;
 for (let i = 0; i <= contador2; i++) {
         console.log(i);
 }
+
+let functionResult = "capuchino";
+function cafetera(ingrediente1, ingrediente2){
+    if (ingrediente1=="leche" && ingrediente2=="cafe") {
+        return functionResult;
+    }
+}
+let taza=cafetera("leche","cafe");
+console.log(taza);*/
+
+const txtn1=document.getElementById("n1");
+const txtn2=document.getElementById("n2");
+const respuesta=document.getElementById("respuesta");
+const botonCalcular=document.getElementById("calcular");
+
+botonCalcular.addEventListener("click", calcular);
+
+function calcular(){
+    const op1=parseFloat(txtn1.value);
+    const op2=parseFloat(txtn2.value);
+    let resp=op1+op2;
+
+    respuesta.innerText=resp;
+    respuesta.style="color:red";
+
+}
+
+const respArr=document.getElementById("respuestaArr");
+const botonEjecutarArr=document.getElementById("ejecuta");
+botonEjecutarArr.addEventListener("click", iniciar);
+
+let datos= ["manzana", 2, false, "naranja"];
+
+function iniciar(){
+    //respArr.innerText=frutas[frutas.length-1]
+    datos.forEach(function(item, index, array){
+        respArr.innerText = array;
+    })
+}
+
+async function obtenerData(){
+    //const response= await fetch("https://api.github.com/repos/hadley/ggplot2/commits");
+    //const json = await response.text();
+    //console.log(JSON.parse(json));
+    await fetch("https://api.github.com/repos/hadley/ggplot2/commits")
+    .then(response=>response.json())
+    .then(json=>console.log(json))
+    .catch(error=>console.log("Fallo",error));
+}
+obtenerData();
