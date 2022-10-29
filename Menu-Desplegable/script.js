f3a6243b943b7b67c73be37341e00212cee58a5a
alex2001bis <alejandro2001bis@gmail.com>
2022-10-29 11:25:54 +0200

    Nuevos ejercicios acabados

---
+++ Menu-Desplegable/script.js
@@ -0,0 +1,6 @@
+const menutoggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+menutoggle.onclick=function(){
+  menutoggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+}
